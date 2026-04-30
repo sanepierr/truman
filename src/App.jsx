1055f35d4e5eedc302bf2f4dce6d629032265f33
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2026-04-30 18:57:51 +0300

    Initial Truman website project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="site-frame">
+      <iframe
+        src="/original-site.html"
+        title="Truman Website Original"
+        className="site-iframe"
+      />
+    </div>
+  )
+}
+
+export default App
